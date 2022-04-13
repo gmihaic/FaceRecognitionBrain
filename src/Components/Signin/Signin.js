0d58fc40fa2f479307dfc15c335dcaca1c1aeeb6
gmihaic <godri.mihai+github@gmail.com>
2022-04-13 22:21:35 +0300

    updates

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -3,36 +3,76 @@ import './Signin.css'
 
 class Signin extends Component {
 
-    constructor(props) {
-       
-        super();  
-        this.props = props;            
+    constructor(props) {       
+        super(props);  
+        this.props = props;   
+        
+        this.state = {
+            signInEmail: '',
+            signInPassword: ''            
+        }         
     }   
+
+    onEmailChange = (event) => {
+        this.setState(
+            {signInEmail: event.target.value}
+        );
+    }
+
+    onPasswordChange = (event) => {
+        this.setState(
+            {signInPassword: event.target.value}
+        );
+    }
+
+    onSubmitSignIn = () => {
+        /*console.log(this.state);
+        this.props.onRouteChange("home");*/
+
+        fetch("http://localhost:3610/signin", {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: this.state.signInEmail,
+                password: this.state.signInPassword
+            })
+        })
+         .then((response) => response.json())
+         .then((data) => {
+             if (data === 'success') {
+                this.props.onRouteChange("home");
+             } else {               
+             }
+         });                  
+    }
            
-    render() {                    
+    render() {           
+        
+        const { onRouteChange } = this.props;
+
         return (
             <>            
                     <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">      
                     <main className="pa4 black-80">
-                        <form className="measure">
+                        <div className="measure">
                             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                                <input onChange={this.onEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
                             </div>
                             <div className="mv3">
                                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                                <input onChange={this.onPasswordChange} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
                             </div>                            
                             </fieldset>
                             <div className="">
-                            <input onClick={() => this.props.onRouteChange('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+                            <input onClick={this.onSubmitSignIn} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
                             </div>
                             <div className="lh-copy mt3">
-                            <p onClick={() => this.props.onRouteChange('register')} className="f6 link dim black db pointer">Register</p>                            
+                            <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>                            
                             </div>
-                        </form>
+                        </div>
                     </main>         
                     </article>                      
             </>
